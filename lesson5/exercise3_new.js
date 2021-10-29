5c17e37c1f82708821eeb02ba225e33bb97cee5a
Ioannis Emmanouil Kosmadakis <kosmadakis.john@gmail.com>
2021-10-29 15:45:02 +0300

    Exercise 3 - New Implementation

--- lesson5/exercise3_new.js
+++ lesson5/exercise3_new.js
@@ -9,6 +9,38 @@ window.addEventListener('load', () => {
     
     // Event listener
     document.getElementById("header1").addEventListener("click", onClick);
+
+    // Destructuring
+
+    const x = {a: 3, b: 4, c: {d : 6}};
+    // const a = x.a;
+    // const b = x.b;
+    const { b, a, c: {d} } = x;
+
+    // -------------
+
+    const newX = {
+      ...x,
+      a: 10,
+      e: 11
+    };
+
+    const myArr = [1,2,3,4];
+    const [el1, el2] = myArr;
+    const [k, m, ...taYpoloipa] = myArr;
+
+    // Why we do not use var and prefer let
+    // for(var i = 0; i < 5; i++) {
+    //     setTimeout(()=>{
+    //         console.log(i);
+    //     }, 3000);
+    // }
+
+    // for(let i = 0; i < 5; i++) {
+    //     setTimeout(()=>{
+    //         console.log(i);
+    //     }, 3000);
+    // }
   });
 
 // (function(){
