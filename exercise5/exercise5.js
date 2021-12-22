ff37fe6ff36a42bbe3ae02fbfb4192603ff3ee72
Ioannis Emmanouil Kosmadakis <kosmadakis.john@gmail.com>
2021-12-22 17:17:16 +0200

    Exercise based on the tic tac tow game

--- exercise5/exercise5.js
+++ exercise5/exercise5.js
@@ -1,8 +1,10 @@
 const gameState = ['X', 'O', 'X', '', '', '', '', '', ''];
 
+
 const gameElement = document.getElementById("game");
 
 gameElement.innerHTML = '<h1>Tic Tac Toe</h1>';
+
 
 const descriptionElement = document.createElement('p');
 descriptionElement.setAttribute('id', 'description');
@@ -13,9 +15,27 @@ descriptionElement.appendChild(descriptionText);
 gameElement.appendChild(descriptionElement);
 
 const boardElement = document.createElement('div');
+
 boardElement.setAttribute('id', 'board');
 boardElement.setAttribute('class', 'board');
 
+
+// gameState.forEach((el, index) => {
+//   const row = Math.floor(index / 3);
+//   const squareElement = document.createElement('div');
+//   squareElement.setAttribute('id', `square-${index}`);
+//   squareElement.setAttribute('class', `square square-of-row-${row}`);
+
+//   const valueTextNode = document.createTextNode(el);
+//   squareElement.appendChild(valueTextNode);
+
+//   boardElement.appendChild(squareElement);
+// });
+
+// gameElement.appendChild(boardElement);
+
+
+// DESIRED
 /* 
   <div id="board" class="board"> 
     <div id="row-0" class="row">
@@ -36,17 +56,28 @@ boardElement.setAttribute('class', 'board');
   </div> 
 */
 
- 
-gameState.forEach((el, index) => {
-  const row = Math.floor(index / 3);
-  const squareElement = document.createElement('div');
-  squareElement.setAttribute('id', `square-${index}`);
-  squareElement.setAttribute('class', `square square-of-row-${row}`);
 
-  const valueTextNode = document.createTextNode(el);
-  squareElement.appendChild(valueTextNode);
+let squaresIndex = 0;
 
-  boardElement.appendChild(squareElement);
-});
+for (let rows = 0; rows < 3; rows++) {
+  const rowElement = document.createElement('div');
+  rowElement.setAttribute('id', `row-${rows}`);
+  rowElement.setAttribute('class', 'square');
+
+  for (let columns = 0; columns < 3; columns++) {
+    const squareElement = document.createElement('div');
+    squareElement.setAttribute('id', `square-${squaresIndex}`);
+    squareElement.setAttribute('class', 'square');
+
+    const valueTextNode = document.createTextNode(gameState[squaresIndex]);
+    squareElement.appendChild(valueTextNode);
+
+    rowElement.appendChild(squareElement);
+
+    squaresIndex++;
+  }
+
+  boardElement.appendChild(rowElement);
+}
 
 gameElement.appendChild(boardElement);
